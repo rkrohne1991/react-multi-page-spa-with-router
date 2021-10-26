a49eee5a6f5ef6729c34e5ff8136d1217185a8b0
rkrohne1991 <rafal.krohne@hotmail.co.uk>
2021-10-26 07:24:03 +0100

    Adding new routes, switch and activating the router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import { Route, Switch } from "react-router-dom";
+
+import AllQuotes from "./pages/AllQuotes";
+import QuoteDetail from "./pages/QuoteDetail";
+import NewQuote from "./pages/NewQuote";
+
 function App() {
-  return <div></div>;
+  return (
+    <Switch>
+      <Route path="/quotes" exact>
+        <AllQuotes />
+      </Route>
+      <Route path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Route>
+      <Route path="/new-quote">
+        <NewQuote />
+      </Route>
+    </Switch>
+  );
 }
 
 export default App;
